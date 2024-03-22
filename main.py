ef646e20f96496589262749ac618737e06bd8d25
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-03-21 21:39:42 -0400

    Add loop to create combine shape

--- main.py
+++ main.py
@@ -1,50 +1,69 @@
 from turtle import Turtle, Screen
-
+import random
 # Create a turtle object
 t = Turtle()
 t.color("green")
 t.width(3)
 
+color_list = ["green","blue","red"]
+
+# Combine Shape
+for i in range(3,10):
+    t.color(random.choice(color_list))
+    for _ in range(i):
+        angle = 180 - (((i-2)*180)/i)
+        t.forward(100)
+        t.left(angle)
+
+
 
 # Triangle
 # for _ in range(3):
+#     t.color(random.choice(color_list))
 #     t.forward(100)
 #     t.left(120)
 
 
 # Square
 # for _ in range(4):
+#     t.color(random.choice(color_list))
 #     t.forward(100)
 #     t.left(90)
 
 # Pentagon
 # for _ in range(5):
+#     t.color(random.choice(color_list))
 #     t.forward(100)
 #     t.left(72)
 
 
 # Hexagon
 # for _ in range(6):
+#     t.color(random.choice(color_list))
 #     t.forward(100)
 #     t.left(60)
 
 # Heptagon
 # for _ in range(7):
+#     t.color(random.choice(color_list))
 #     t.forward(100)
 #     t.left(51.43)
 
 # Octagon
 # for _ in range(8):
+#     t.color(random.choice(color_list))
 #     t.forward(100)
 #     t.left(45)
 
 # Nonagon
 # for _ in range(9):
+#     t.color(random.choice(color_list))
 #     t.forward(100)
 #     t.left(40)
 
 # Decagon
 # for _ in range(10):
+#     t.color(random.choice(color_list))
 #     t.forward(100)
 #     t.left(36)
 
